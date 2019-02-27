ff8a722754a1ec2c35c1d05e1be64dd28c4debee
cdk-king <1360531956@qq.com>
2019-02-28 02:24:53 +0800

    定义基本的关卡元数据

---
+++ js/maps.js
@@ -0,0 +1,12 @@
+var maps = {
+    "singleplayer":[
+        {
+            "name":"Introduction",
+            "briefing":"In this level you will learn how to pan across the map.\n\nDon't worry! We will be implementing more features soon.",
+            // 地图细节
+            "mapImages":"images/maps/plains-debug.png",
+            "startX":4,
+            "startY":4,
+        },
+    ]
+}
