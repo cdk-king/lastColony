fdbd38304a10c4de793e86b2b63f1ba35c979009
cdk-king <1360531956@qq.com>
2019-03-02 04:18:27 +0800

    定义单位 定义主基地 添加建筑类型 添加单位创建和加载方法

---
+++ js/buildings.js
@@ -0,0 +1,52 @@
+var building = {
+    list:{
+        "base":{
+            name:"base",
+            //绘图属性
+            pixelWidth:60,
+            pixelHeight:60,
+            baseWidth:40,
+            baseHeight:40,
+            pixelOffsetX:0,
+            pixelOffsetY:20,
+            //描述查找路线算法结构的属性
+            buildableGrid:[
+                [1,1],
+                [1,1]
+            ],
+            passableGrid:[
+                [1,1],
+                [1,1]
+            ],
+            sight:3,
+            hitPosition:500,
+            cost:5000,
+            spriteImages:[
+                {name:"healthy",count:4},
+                {name:"damaged",count:1},
+                {name:"contructing",count:3},
+            ],
+        }
+    },
+    defaults:{
+        type:"buildings",
+        animationIndex:0,
+        direction:0,
+        orders:{
+            type:"stand"
+        },
+        action:"stand",
+        selected:false,
+        selectable:true,
+        //建筑的默认动画循环
+        animate:function(){
+
+        },
+        //建筑的默认绘图循环
+        draw:function(){
+
+        }
+    },
+    load:loadItem,
+    add:addItem
+}
--- js/common.js
+++ js/common.js
@@ -88,3 +88,87 @@ var loader = {
         }
     }
 }
+
+// 默认的load()方法被游戏中的所有单位使用
+function loadItem(name){
+    //this指向的也只是它上一级的对象
+    var item = this.list[name];
+    //如果单位的子画面序列已经加载，就没必要在此加载了
+    //this指向的也只是它上一级的对象
+    if(item.spriteArray){
+        return;
+    }
+    item.spriteSheet = loader.loadImage("images/"+this.defaults.type+"/"+name+".png");
+    item.spriteArray = [];
+    item.spriteCount = 0;
+
+    for(var i = 0;i<spriteImages.length;i++){
+        var constructImageCount = item.spriteImages[i].count;
+        var constructDirectionCount = item.spriteImages[i].directions;
+        if(constructDirectionCount){
+            for(var j = 0;j<constructDirectionCount;j++){
+                var constructImageName = item.spriteImages[i].name+"-"+j;
+                item.spriteArray[constructImageName] = {
+                    name:constructImageName,
+                    count:constructImageCount,
+                    offset:item.spriteCount
+                };
+                item.spriteCount += constructImageCount;
+            }
+        }else{
+            var constructImageName = item.spriteImages[i].name;
+            item.spriteArray[constructImageName] = {
+                name:constructImageName,
+                count:constructImageCount,
+                offset:item.spriteCount
+            };
+            item.spriteCount += constructImageCount;
+        }
+    }  
+}
+////对于仍不支持object.assign的一些浏览器，polyfill
+//Object.assign(target, ...sources)方法用来将源对象（source）的所有可枚举属性，复制到目标对象（target）。
+if(typeof Object.assign!=="function"){
+    //垫片
+    Object.assign = function(target, varArgs) { 
+        "use strict";//严格模式
+        if (target === null) { // TypeError if undefined or null
+            throw new TypeError("Cannot convert undefined or null to object");
+        }
+        var to = Object(target);//new Object([value])
+
+        for (var index = 1; index < arguments.length; index++) {
+            var nextSource = arguments[index];
+
+            if (nextSource != null) { // Skip over if undefined or null
+                for (var nextKey in nextSource) {
+                    // Avoid bugs when hasOwnProperty is shadowed
+                    //判断一个属性是定义在对象本身而不是继承自原型链
+                    //使用原型链上真正的 hasOwnProperty 方法
+                    if (Object.prototype.hasOwnProperty.call(nextSource, nextKey)) {
+                        to[nextKey] = nextSource[nextKey];
+                    }
+                }
+            }
+        }
+
+        return to;
+        
+    }
+}
+
+// 默认的add()方法被游戏中所有的单位使用
+function addItem(details){
+    var item = {};
+    var name = details.name;
+
+    Object.assign(item, this.defaults);
+
+    Object.assign(item, this.list[name]);
+
+    item.life = item.hitPoints;
+
+    Object.assign(item, details);
+
+    return item;
+}
